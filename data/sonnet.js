8802ca81f8d1a7badbae51c08d0a1f4371d797f2
Duncan MacKenzie <duncan@duncanmacweb.com>
2015-01-18 17:40:52 +0000

    Add dummy words data; move Sonnet 18 to data/ and update package.json

--- data/sonnet.js
+++ data/sonnet.js
@@ -1,4 +1,5 @@
-var sonnet = `Shall I compare thee to a summer's day?
+module.exports = { sonnet18:
+`Shall I compare thee to a summer's day?
 Thou art more lovely and more temperate:
 Rough winds do shake the darling buds of May,
 And summer's lease hath all too short a date;
@@ -11,14 +12,4 @@ Nor lose possession of that fair thou ow'st;
 Nor shall Death brag thou wander'st in his shade,
 When in eternal lines to time thou grow'st:
 So long as men can breathe or eyes can see,
-So long lives this, and this gives life to thee.`
-
-var lines = sonnet.split('\n')
-
-var words = Array.prototype.concat(...lines.map(line => line.split(' ')))
-
-console.log(words.join('-'))
-
-var firstletters = words.map(word => word[0].toLowerCase()).join('')
-
-console.log(firstletters)
+So long lives this, and this gives life to thee.` }
