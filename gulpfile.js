5624005c3dbb8cf610b2c3d60b158285909e35df
Duncan MacKenzie <duncan@duncanmacweb.com>
2015-01-18 16:13:08 +0000

    Amend gulpfile to transform any js file in lib/

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var to5 = require('gulp-6to5');
 
 gulp.task('default', function () {
-    return gulp.src('lib/sonnet.js')
+    return gulp.src('lib/**/*.js')
         .pipe(to5())
         .pipe(gulp.dest('dist'));
 });
