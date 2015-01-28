6ca3a42ab6fc7f4b2df53f2dc84e6a31ca735f22
Duncan MacKenzie <duncan@duncanmacweb.com>
2015-01-28 11:58:24 +0000

    Add sourcemaps to gulp and 6to5, reintroduce bug to test\!

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
 var to5 = require('gulp-6to5');
 
 gulp.task('default', function () {
     return gulp.src('lib/**/*.js')
+    	.pipe(sourcemaps.init())
         .pipe(to5())
         .pipe(gulp.dest('dist'));
 });
